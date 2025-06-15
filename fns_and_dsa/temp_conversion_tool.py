3c9bc31786e94049ea357186fac86372f82b7027
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-06-15 11:29:29 +0000

    Fixed some grammatical errors

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,18 +1,18 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-def convert_to_celcius(farenheit):
-    return (farenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+def convert_to_celcius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
-def convert_to_farenheit(celcius):
+def convert_to_fahrenheit(celcius):
     return (celcius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 temperature = float(input("Enter the temperature to convert: "))
-degree = str(input("Is this temperature in Celcius or Farenheit? (C/F):"))
+degree = str(input("Is this temperature in Celcius or Fahrenheit? (C/F): "))
 
 if degree == "F":
     print(f"{temperature}°F is {convert_to_celcius(temperature)}°C")
 elif degree == "C":
-    print(f"{temperature}°C is {convert_to_farenheit(temperature)}°F")
+    print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
 else:
     print("Invalid temperature. Please enter a numeric value")
