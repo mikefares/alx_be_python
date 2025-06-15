dd1ab75fa45d02d8b2d21ba7bec330ae09d238e8
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-06-15 10:58:54 +0000

    Corrected errors on datetime exercise

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,7 +1,7 @@
-import datetime
+from datetime import datetime, timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now()
+    current_date = datetime.now()
     formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S")
     print("Current date and time: " + formatted_datetime)
 
@@ -10,8 +10,8 @@ display_current_datetime()
 number_of_days = int(input("Enter the number of days to add to the current date: "))
 
 def calculate_future_date():
-    current_date = datetime.datetime.now()
-    future_date = current_date + datetime.timedelta(days=number_of_days)
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=number_of_days)
     formatted_future_date = future_date.strftime("%Y-%m-%d")
 
     print("Future date: " + formatted_future_date)
