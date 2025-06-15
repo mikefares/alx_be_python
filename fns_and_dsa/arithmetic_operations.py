c7b77f9873a70c6a16db150fd1f3b2ab50c04e3e
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-06-15 01:14:22 +0000

    Corrected arithmetic operation errors

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,14 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 != 0:
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
                 return num1 / num2
-            else:
-                print("Cannot divide by zero")
-
+        else:
+            print("Cannot divide by zero")
+    else:
+        print("Operation not valid")
