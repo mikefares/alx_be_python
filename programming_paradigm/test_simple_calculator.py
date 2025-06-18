082e454daabcddda8fc42f3870049b8cb7e67837
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-06-18 20:43:06 +0000

    Simple calculator test exercise

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,26 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(5,3), 8)
+        self.assertEqual(self.calc.add(-3,3), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5,3), 2)
+        self.assertEqual(self.calc.subtract(-5,3), -8)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(5,6), 30)
+        self.assertEqual(self.calc.multiply(5,-6), -30)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(15,3), 5)
+        self.assertEqual(self.calc.divide(20,5), 4)
+
+    def test_divide_by_zero(self):
+        self.assertIsNone(self.calc.divide(10,0))
+        self.assertIsNone(self.calc.divide(0,0))
