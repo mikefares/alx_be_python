2f01f152b7e183b840356837f68ed9316667a0dc
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-06-22 23:47:41 +0000

    Added library management file

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,65 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self._is_checked_out = False
+
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self._books.append(book)
+            print(f"Added '{book.title}' to the library.")
+        else:
+            print("Can only add instances of Book class.")
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.check_out():
+                    print(f"'{title}' has been checked out.")
+                    return True
+                else:
+                    print(f"'{title}' is already checked out.")
+                    return False
+        print(f"'{title}' not found in the library.")
+        return False
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title:
+                if book.return_book():
+                    print(f"'{title}' has been returned.")
+                    return True
+                else:
+                    print(f"'{title}' was not checked out.")
+                    return False
+        print(f"'{title}' not found in the library.")
+        return False
+
+    def list_available_books(self):
+        available_found = False
+        for book in self._books:
+            if not book._is_checked_out:
+                print(book)
+                available_found = True
+        if not available_found:
+            print("No books available.")
