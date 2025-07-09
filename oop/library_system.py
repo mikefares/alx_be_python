653cfa17f75ac84d12b9c0b5826c08a207b657e1
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-07-09 20:02:31 +0000

    Added library system exercise

---
+++ oop/library_system.py
@@ -0,0 +1,27 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        
+class EBook(Book):
+    def __init__(self, title, author, file_size: int):
+        super().__init__(title, author)
+
+        self.file_size = file_size
+
+class PrintBook(Book):
+    def __init__(self, title, author, page_count: int):
+        super().__init__(title, author)
+
+        self.page_count = page_count
+
+class Library:
+    def __init__(self):
+        self.book = []
+
+    def add_book(self, book):
+        self.book.append(book)
+
+    def list_books(self):
+        for book in self.book:
+            print(book)
