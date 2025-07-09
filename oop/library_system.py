5e618f8fbce3ef260110e02384f54573f379818e
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-07-09 20:12:57 +0000

    Corrected some errors on library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -13,7 +13,7 @@ class EBook(Book):
         self.file_size = file_size
 
     def __str__(self):
-        return f"{super().__str__()}, File Size: {self.file_size}KB "
+        return f"{super().__str__()}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count: int):
