0f7e6b57dea5c03bcad442dc2476fe012e8778a0
Michael Fares Zietouny <mickeyzietounyjnr@gmail.com>
2025-07-09 19:39:24 +0000

    Corrected some errors in book class file

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,14 +1,14 @@
 class Book:
-    def __init__(self, title: str, author: str, publication_year: int):
+    def __init__(self, title: str, author: str, year: int):
         self.title = title
         self.author = author
-        self.publication_year = publication_year
+        self.year = year
 
     def __del__(self):
         print(f"Deleting {self.title}")
 
     def __str__(self):
-        return f"{self.title} by {self.author}, published in {self.publication_year}"
+        return f"{self.title} by {self.author}, published in {self.year}"
     
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}',{self.publication_year})"
+        return f"Book('{self.title}', '{self.author}',{self.year})"
